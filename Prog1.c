adf3d36bd1cbc8109d6c5769ffc5da86935fa3d0
Mateus Corrente <mateusbotelho0911@gmail.com>
2020-01-06 09:35:34 -0100

    Programa que calcula Receitas/Despesas de um mês

---
+++ Prog1.c
@@ -0,0 +1,53 @@
+// Programa que calcula Receitas/Despesas de um mês //
+#include <stdio.h>
+
+// Declaração de variáveis globais //
+
+double receita;
+double despesas;
+double saldo;
+
+int main() {
+
+valorReceita();
+valorDespesas();
+calcSaldo();
+
+
+printf("Valor para as receitas: %2.lf\n\nValor para as despesas: %2.lf\n\n Saldo: %2.lf\n", receita, despesas, saldo);
+return 0;
+}
+
+/*=========================================================================*/
+
+        // Função de pergunta nº1 para definir a variavel receita
+        
+valorReceita(void) {
+
+printf("Insira o valor para as receitas: ");
+scanf("%lf/n", &receita);
+
+}
+
+/*=========================================================================*/
+
+        // Função de pergunta nº2 para definir a variavel despesas
+
+valorDespesas(void) {
+
+printf("Insira o valor para as despesas: ");
+scanf("%lf/n", &despesas);
+
+}
+
+/*=========================================================================*/
+
+                     // Função para calcular o saldo
+
+calcSaldo(void) {
+
+saldo = (receita - despesas);
+
+}
+
+/*=========================================================================*/
